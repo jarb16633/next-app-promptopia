2535b1e61bebd5f780b1ce70d6fd2f194b5f6288
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2024-09-18 20:52:03 +0700

    upload

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import Nav from "@components/Nav";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and share AI prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
